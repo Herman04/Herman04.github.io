bd6e31bf56b075e497973a79b6d9875eaf8c456d
Herman04 <hermanchenwi@Hermans-MacBook-Pro.local>
2016-01-31 15:05:07 -0500

    Work done for today
    It’s been a great day but more work needs to be done

---
+++ Java101.js
@@ -0,0 +1,11 @@
+var temperature = 25;
+if (temperature < 30)  {
+	console.log("wear a hat");
+
+} 
+else if (temperature < 50) {
+	console.log("wear a hat and a jacket");
+}
+else{
+	console.log("wear whatever you want.");
+}
